18ea526d515410d101a700786ea874fa43686f86
Stephen Thompson <Stephen.Revel.Thompson@gmail.com>
2021-08-01 20:04:01 -0400

    Init Commit

---
+++ README.md
@@ -0,0 +1,42 @@
+References:
+- https://github.com/polm/fugashi#installing-a-dictionary
+- https://github.com/PokiDokika/jisho-py
+
+Subtitle Sites:
+- https://kitsunekko.net
+
+# How it works
+Download a japanese subtitle file. This tool will parse that file and give you the n most common japanese words used. These words will be exported to a new subtitle file with the same time stamps, but only where those words occur.
+
+# Dependencies
+- fugashi - package for japanese language tokenization (I'm using it to break the corpus in to separate words)
+
+Currently not using jisho, but may use later to filter out words not found in the dictionary:
+- jisho-py - package creates a python api to jisho.org japanese dictionary and returns results with japanese kanji & kana as well as english translations
+Note: jisho-py currently must be copied to the site-packages directory because it's pip installation is broken. Just import a package like import numpy as np, then `print(np.__file__)` to get the site-packages location
+
+# Usage
+- Export subtitle file with the 10 most common words (default)
+    ```
+    python subjaptop.py
+    ```
+- Export subtitle file with the 20 most common words
+    ```
+    python subjaptop.py --top 20
+    ```
+- Export subtitle file with the 20 most common words
+    ```
+    python subjaptop.py --top 20
+    ```
+    
+### optional arguments:
+```
+  -s, --sub Subtitle path. If arg not used, will process all files in Subtitles dir
+  -t, --top Get the top n most common words. Default 10.
+  -d, --drop_kana Dont append kana to exported words.
+  -i, --ignore_added Exports any added words to the ignore list. Default False
+``` 
+
+Notes
+- All words will automatically be added to the ignore list under the `IGNORE_LIST/previous_export_words.txt` unless the `--ignore_added True` argument is used.
+- The anki deck's card answer will be limited to how many lines are added with the `--max_lines` arg (default 10). The code will continue to iterate words and definitions on jisho.org until the max line count is reached.
---
+++ subjaptop.py
@@ -0,0 +1,133 @@
+import fugashi
+from fugashi import Tagger
+import argparse
+import os
+from collections import Counter
+import random
+import ntpath
+
+# defaults
+N_MOST_COMMON_WORDS = 10 # Top n most common words
+INCLUDE_KANA = True # Include on Anki card's Question next to Kanji
+INCLUDE_TEXT = False # Include full subtitle
+IGNORE_ADDED = False
+DEFAULT_DECK_NAME = 'exported.apkg'
+IGNORE_FILENAME = 'previous_export_words.txt'
+OUTPUT_DIR = 'New_Subs'
+SUB_DIR = 'Subtitles'
+IGNORE_DIR = 'Ignore_Lists'
+valid_extensions = ['.srt']
+
+# Intialization
+tagger = Tagger()
+for dir_name in [SUB_DIR, IGNORE_DIR, OUTPUT_DIR]:
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
+        
+# Import ignore list
+ignore = []
+for ignore_file in [f for f in os.listdir(IGNORE_DIR) if f.endswith('.txt')]:
+    ignore_file = os.path.join(IGNORE_DIR, ignore_file)
+    with open(ignore_file, 'r') as file:
+        for line in file:
+            ignore += [line.replace('\n', '')]
+
+parser = argparse.ArgumentParser(description='Convert a japanese subtitle file to a list of the most common words from that file & export as Anki flash card deck.')
+parser.add_argument('-s', '--sub', type=str, help='Subtitle path. If arg not used, will process all files in Subtitles dir')
+parser.add_argument('-t', '--top', type=int, help='Get the top n most common words. Default 10.')
+parser.add_argument('-d', '--drop_kana', action='store_true', help='Dont append kana to exported words.')
+parser.add_argument('-i', '--ignore_added', action='store_true', help='Exports any added words to the ignore list. Default False')
+parser.add_argument('-list', '--export_list', action='store_true', help='Exports any added words to the ignore list')
+
+args = parser.parse_args()
+include_kana = not(args.drop_kana)
+n_most_common = args.top if args.top else N_MOST_COMMON_WORDS
+process_all = args.sub == None
+
+if process_all:
+    sub_files = [os.path.join(SUB_DIR, f) for f in os.listdir(SUB_DIR) if f.endswith('|'.join(valid_extensions))]
+else:
+    sub_files = [args.sub]
+    
+for sub_idx, sub_file in enumerate(sub_files):       
+    
+    """
+    Part 1: Get n most common Words --------------
+    """
+
+    all_words = []
+    subs = []
+    body = []
+    with open(sub_file, 'r') as file:
+        for i, line in enumerate(file):
+            line = line.replace('\ufeff', '')
+            if line in ['', ' ']:
+                continue
+
+            # Parse sub as list of tuples (timestamps, words)
+            if line.strip().isdigit():
+                if i > 0:
+                    subs += [(body[0], body[1:])]
+                body = []
+            else:
+                body += [line]
+
+            # Split japanese words
+            for word in tagger(line):
+                all_words += [word.surface]
+     
+    filtered = [w for w in all_words if w not in ['　'] and w.split()[0] not in ignore and not w.isdigit()]
+    word_counts = Counter(filtered)
+    
+    mc = [word.split()[0] for word, _ in word_counts.most_common()[:n_most_common]]
+    print(''.join([str(w) + '\n' for w in word_counts.most_common()[:n_most_common]]))
+
+    # Export new subtitle file with just most common words
+    new_sub = []
+    idx = 1
+    # Iterate subs in sub file
+    for time, body in subs:
+        match_cnt = 0
+        for line in body:
+            for word in tagger(line):
+                if word.surface in mc:
+                    if match_cnt == 0:
+                        new_sub += [str(idx)+'\n'] + [time]
+
+                    kana_str = ''
+                    if include_kana and word.feature.kana:
+                        kana_str = '('+word.feature.kana+')'
+
+                    new_sub += [f'{word.surface} {kana_str}\n']
+                    match_cnt += 1
+
+        if match_cnt > 0:
+            if INCLUDE_TEXT:
+                new_sub += body 
+            else:
+                new_sub += ['\n']
+            idx += 1 
+
+        export_name = ntpath.basename(sub_file).split('.')[0] + '_Top' + str(n_most_common) + '.srt'
+        with open(os.path.join(OUTPUT_DIR, export_name), 'w') as file:
+            file.writelines([w for w in new_sub])
+
+    # Export words to ignore for future decks
+    if IGNORE_ADDED:
+        IGNORE_FILENAME = 'previous_export_words.txt'
+        ignore_file = os.path.join(IGNORE_DIR, IGNORE_FILENAME)
+        new_ignores = skipped + words_added
+
+        if os.path.isfile(ignore_file):
+            with open(ignore_file, 'r') as file:
+                for line in file:
+                    new_ignores += [line.replace('\n', '')]
+
+        new_ignores = sorted(set(new_ignores))
+
+        with open(ignore_file, 'w') as file:
+            file.writelines([w + '\n' for w in new_ignores])
+    
+    ignored = '' if IGNORE_ADDED else ' not'
+    print(f'Export complete... Most common words were{ignored} added to the ignore list.')
+
